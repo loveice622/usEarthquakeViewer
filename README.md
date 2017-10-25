9554211ad2a83c1d2014f2ddd13f1ef29a4fe823
loveice622 <bzhao@wpi.edu>
2017-10-24 23:49:16 -0400

    Modify source files related to project

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+##Introduction
+
+
+This demo shows the D3 interaction with the time line slider. The x axis of the slider shows the date of data. User is able to drag the slider to change the date across the axis and the map will display the data in scatters before this date.  
+
+Interaction element is refered from [Mike Bostock’s Blocks](https://bl.ocks.org/mbostock/6452972)
+
+
+This data is from [The USGS Earthquake Hazards Program](https://earthquake.usgs.gov/earthquakes/search/).
+
+This dataset is about U.S earthquake incidents from year 1900 which the magnitude of the earthquake are larger than 5.5
+
+forked from <a href='http://bl.ocks.org/loveice622/'>loveice622</a>'s block: <a href='http://bl.ocks.org/loveice622/ef51da75b1c14f8373abb3162da3f0d9'>USA earthquake data from year 1900</a>
+
+
+
+forked from <a href='https://bl.ocks.org/mbostock/'>Mike Bostock</a>'s block: <a href='https://bl.ocks.org/mbostock/3734308'>earth-USA earthquake data from year 1900</a>
+
+
+
+
 A template project that uses Webpack and D3. Designed as a starting point for interactive data visualization projects that require JavaScript code to be organized across many files (as ES6 modules).
 
 The starter code here is from [Stylized Scatter Plot with Color Legend](https://bl.ocks.org/curran/ecb09f2605c7fbbadf0eeb75da5f0a6b).
--- src/index.js
+++ src/index.js
@@ -1,50 +1,156 @@
-import scatterPlot from './scatterPlot'
+var width = 960,
+    height = 550;
+  
+margin = {right: 50, left: 50}
 
-const xValue = d => d.sepalLength;
-const xLabel = 'Sepal Length';
-const yValue = d => d.petalLength;
-const yLabel = 'Petal Length';
-const colorValue = d => d.species;
-const colorLabel = 'Species';
-const margin = { left: 120, right: 300, top: 20, bottom: 120 };
+var projection = d3.geoAlbers()
+    .rotate([96, 0])
+    .center([-.6, 35.7])
+    .parallels([37.5, 45.5])
+    .scale(800)
+    .translate([width / 2, height / 2])
+    .precision(.1);
 
-const visualization = d3.select('#visualization');
-const visualizationDiv = visualization.node();
-const svg = visualization.select('svg');
+var path = d3.geoPath()
+    .projection(projection);
 
-const row = d => {
-  d.petalLength = +d.petalLength;
-  d.petalWidth = +d.petalWidth;
-  d.sepalLength = +d.sepalLength;
-  d.sepalWidth = +d.sepalWidth;
-  return d;
-};
+var graticule = d3.geoGraticule()
+    .extent([[-98 - 45, 38 - 45], [-98 + 45, 38 + 45]])
+    .step([5, 5]);
 
-d3.csv('data/iris.csv', row, data => {
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+  
+svg.append('text')
+        .attr('class', 'head')
+        .attr('x', 230)
+        .attr('y', 20)
+        .text('USA earthquake data from year 1900');
+  
 
-  const render = () => {
+svg.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
 
-    // Extract the width and height that was computed by CSS.
-    svg
-      .attr('width', visualizationDiv.clientWidth)
-      .attr('height', visualizationDiv.clientHeight);
+d3.json("https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json", function(error, us) {
+  if (error) throw error;
 
-    // Render the scatter plot.
-    scatterPlot(svg, {
-      data,
-      xValue,
-      xLabel,
-      yValue,
-      yLabel,
-      colorValue,
-      colorLabel,
-      margin
-    });
-  }
+  svg.insert("path", ".graticule")
+      .datum(topojson.feature(us, us.objects.land))
+      .attr("class", "land")
+      .attr("d", path);
 
-  // Draw for the first time to initialize.
-  render();
 
-  // Redraw based on the new size whenever the browser window is resized.
-  window.addEventListener('resize', render);
+
+  svg.insert("path", ".graticule")
+      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+      .attr("class", "state-boundary")
+      .attr("d", path);
 });
+  
+  
+var parseTime  = d3.timeParse("%Y-%m-%d")   
+var formatTime = d3.timeFormat("%Y");    
+  
+d3.csv("data/us-earthquake-from1900.csv", function(d){
+    
+
+
+    
+   // range to operate on
+var extent = d3.extent(d, function(d){ 
+    return parseTime(d.time.split('T')[0]); 
+});
+   
+  // console.log(extent)
+   
+  formatDate = d3.timeFormat("%Y");
+    
+   // scale function
+   var x = d3.scaleTime()
+      .domain(extent)
+      .range([0, width-100])
+      .clamp(true);
+
+
+    // initial value
+    var startValue = x(new Date('2012-03-20'));
+    startingValue = new Date('2012-03-20');
+    
+
+   // var x = d3.scaleLinear()
+     //   .domain([0, 180])
+       // .range([0, width])
+        //.clamp(true);
+
+    var slider = svg.append("g")
+        .attr("class", "slider")
+        .attr("transform", "translate(" + margin.left + "," + (height-100) + ")");
+
+    slider.append("text")
+    .attr("x",0)
+    .attr("y",-20)
+    .text("Show earthquakes before selected year")
+    
+    
+    slider.append("line")
+        .attr("class", "track")
+        .attr("x1", x.range()[0])
+        .attr("x2", x.range()[1])
+      .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+        .attr("class", "track-inset")
+      .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+        .attr("class", "track-overlay")
+        .call(d3.drag()
+            .on("start.interrupt", function() { slider.interrupt(); })
+            .on("start drag", function() { updateData(x.invert(d3.event.x),d); }));
+
+    slider.insert("g", ".track-overlay")
+        .attr("class", "ticks")
+        .attr("transform", "translate(0," + 18 + ")")
+      .selectAll("text")
+      .data(x.ticks(10))
+      .enter().append("text")
+        .attr("x", x)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatDate(d); });
+
+    var handle = slider.insert("circle", ".track-overlay")
+        .attr("class", "handle")
+        .attr("r", 10);
+
+    function updateData(h,d) 
+    {
+      console.log(formatDate(h))
+           
+      handle.attr("cx", x(h));
+      
+      svg.selectAll('circle[class=scatter]').remove()
+      
+      for(i = 0; i < d.length; i ++){
+    
+           var mystring =d[i].time
+           filterYear=parseTime(mystring.split('T')[0])
+           //console.log(formatTime(d.time))
+           
+           //if(formatTime(filterYear)==formatTime(h))
+           if(parseInt(formatTime(filterYear))<=parseInt(formatTime(h)))
+           
+            {
+
+              svg.append("circle")
+                .attr("cx", projection([d[i].longitude,d[i].latitude])[0])
+                .attr("cy", projection([d[i].longitude,d[i].latitude])[1])
+                .attr("r", (d[i].mag-5)*4)
+                .style("fill", "red")
+                .attr("class", "scatter")
+
+              }
+            }
+           
+    }
+
+    
+})
